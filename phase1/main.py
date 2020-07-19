627b9d81053728542b67e389b9db2ba8044f54fe
leon-wulin <55013445+leon-wulin@users.noreply.github.com>
2020-07-19 17:46:14 +0800

    updated

--- phase1/main.py
+++ phase1/main.py
@@ -2,8 +2,8 @@
 # writen by leon
 # writen time: 2019/11/23
 
-from class_equipment import Equipment
-from eq_attack import eq_attack
+from pacageKOG.class_equipment import Equipment
+from pacageKOG.eq_attack import eq_attack
 
 
 eq1=Equipment()
